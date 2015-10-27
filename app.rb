b359c1b405d5667dad3a918903004a3661c63b5f
fergus lemon <fergus.lemon@gmail.com>
2015-10-27 10:52:07 +0000

    included index.erb

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'shotgun'
 
 get '/' do
-  "hello!"
+ erb(:index)
 end
 
 get '/secret' do
@@ -11,10 +11,4 @@ end
 
 get '/chuka' do
   "Chuka"
-end
-
-get '/cat' do
-  "<div style ='border: 3px dashed red'>
-  <img src= 'http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'></img>
-  </div"
 end
