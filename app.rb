8fc44d4b152a73ae62931891693976531d9a16ce
Chuka Ebi <chuka.ebi@outlook.com>
2015-10-27 11:42:53 +0000

    .

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 require 'shotgun'
 
 get '/' do
+ "hello"
+end
+
+get '/named-cat' do
+  p params
+ @name_randomizer = params[:name]
+ erb(:index)
+end
+
+get '/random-cat' do
  @name_randomizer = %w(Amigo Oscar Viking).sample
  erb(:index)
 end
